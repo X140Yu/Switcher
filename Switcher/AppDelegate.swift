995e67c99857944df3dfcd1f935f5080437321a9
X140Yu <zhaoxinyu1994@foxmail.com>
2016-05-28 09:54:34 +0800

    Create project

---
+++ Switcher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Switcher
+//
+//  Created by X140Yu on 5/28/16.
+//  Copyright © 2016 X140Yu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
