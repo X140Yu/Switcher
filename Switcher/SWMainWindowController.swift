00e4a9daba3a8154e6dff0c975467f0b0f5bee4e
X140Yu <zhaoxinyu1994@gmail.com>
2016-07-05 22:30:47 +0800

    hide Mainwindow Title

---
+++ Switcher/SWMainWindowController.swift
@@ -0,0 +1,23 @@
+//
+//  SWMainWindowController.swift
+//  Switcher
+//
+//  Created by X140Yu on 7/5/16.
+//  Copyright © 2016 X140Yu. All rights reserved.
+//
+
+import Cocoa
+
+class SWMainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        
+        // http://blog.seedlab.io/currencyx-like-window/
+        if let window = self.window {
+            window.titleVisibility = .Hidden
+            window.titlebarAppearsTransparent = true
+        }
+    }
+
+}
